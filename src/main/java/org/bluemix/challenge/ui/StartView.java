df5923b2e864251eee19bd2324bf9977c304aa88
Marco Collovati <mcollovati@gmail.com>
2015-11-24 22:04:59 +0100

    revisione testi informativi e piccoli aggiustamenti (closes #14)

--- src/main/java/org/bluemix/challenge/ui/StartView.java
+++ src/main/java/org/bluemix/challenge/ui/StartView.java
@@ -10,6 +10,7 @@ import com.vaadin.ui.UI;
 import org.vaadin.cdiviewmenu.ViewMenuItem;
 import org.vaadin.viritin.button.PrimaryButton;
 import org.vaadin.viritin.label.RichText;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
 /**
@@ -17,16 +18,17 @@ import org.vaadin.viritin.layouts.MVerticalLayout;
  */
 @CDIView(value = StartView.VIEW_NAME)
 @ViewMenuItem(order = 0, title = "Start", icon = FontAwesome.HOME)
-public class StartView extends MVerticalLayout implements View{
+public class StartView extends MVerticalLayout implements View {
 
     public static final String VIEW_NAME = "";
 
     public StartView() {
         withMargin(true).withFullWidth();
-        setStyleName("start-view");
+        setStyleName("start-view two-columns");
         addComponent(new RichText().withMarkDown(getClass().getResourceAsStream("start.md")));
         addComponent(new PrimaryButton("Start here",
                 event -> UI.getCurrent().getNavigator().navigateTo(UploadView.VIEW_NAME)));
+        addComponent(new RichText().withMarkDown(getClass().getResourceAsStream("services.md")));
         expand(new CssLayout());
         setHeightUndefined();
 
---
+++ src/main/resources/org/bluemix/challenge/ui/services.md
@@ -0,0 +1,14 @@
+### &nbsp; 
+
+More information on IBM Bluemix services:
+
+* [Visual Recognition documentation](http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/visual-recognition.html)
+* [Visual Insights](http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/visual-insights.html)
+* [Twitter Insights]()
+
+Used Vaadin addons:
+
+* [Vaadin Charts](https://vaadin.com/add-ons/charts)
+* [Viritin](https://vaadin.com/directory#!addon/viritin)
+* [Vaadin CDI](https://vaadin.com/directory#!addon/vaadin-cdi)
+* [cdi-helpers](https://vaadin.com/directory#!addon/cdi-helpers)
